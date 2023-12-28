43ffb125c87181f95a272897c449cc4154aee30b
Asad <stormyboye5987@gmail.com>
2023-12-28 04:00:13 -0800

    destini

--- android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
@@ -1,13 +1,13 @@
 package co.appbrewery.destinichallengestarting;
+import io.flutter.embedding.android.FlutterActivity;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+// import android.os.Bundle;
+// import io.flutter.app.FlutterActivity;
+// import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(savedInstanceState);
+  // }
 }
